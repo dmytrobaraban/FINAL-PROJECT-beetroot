be44a488fc200104596e6dcfdfbc5fe026cbc95e
Dmytro Baraban <79997173+dmytrobaraban@users.noreply.github.com>
2022-11-29 11:51:42 +0200

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -86,7 +86,7 @@ let mySwiper = new Swiper('.swiper2', {
 // timer
 
 // Set the date we're counting down to
-let countDownDate = new Date("September 27, 2022 21:00:00").getTime();
+let countDownDate = new Date("September 27, 2023 21:00:00").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function () {
